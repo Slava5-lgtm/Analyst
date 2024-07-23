89ffb76fff879862fd2be0cefc10ab383c9ee034
Вячеслав <vaceslavsergunin@gmail.com>
2024-07-23 22:27:55 +0500

    +

---
+++ Урок 1. Лекция. Знакомство с языком программирования Python/main7.py
@@ -0,0 +1,15 @@
+iter = 2
+iter += 3 # iter = iter + 3
+print(iter)
+iter -= 4 # iter = iter - 4
+print(iter)
+iter *= 5 # iter = iter * 5
+print(iter)
+iter /= 5 # iter = iter / 5
+print(iter)
+iter //= 5 # iter = iter // 5
+print(iter)
+iter %= 5 # iter = iter % 5
+print(iter)
+iter **= 5 # iter = iter ** 5
+print(iter)
