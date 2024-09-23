d715258423fed1703c74212be509b5279281fadd
Вячеслав <vaceslavsergunin@gmail.com>
2024-09-23 10:55:41 +0500

    +

--- Знакомство с языком Python. Обучение в записи/Урок 10. Лекция. Функции высшего порядка, работа с файлами/main4.py
+++ Знакомство с языком Python. Обучение в записи/Урок 10. Лекция. Функции высшего порядка, работа с файлами/main4.py
@@ -1,11 +1,3 @@
-users = ['user1', 'user2', 'user3', 'user4', 'user5']
-ids = [4, 5, 9, 14, 7]
-data = list(zip(users, ids))
-print(data) # [('user1', 4), ('user2', 5), ('user3', 9), ('user4', 14), ('user5', 7)]
-
-# Функция zip () пробегает по минимальному входящему набору:
-users = ['user1', 'user2', 'user3', 'user4', 'user5']
-ids = [4, 5, 9, 14, 7]
-salary = [111, 222, 333]
-data = list(zip(users, ids, salary))
-print(data) # [('user1', 4, 111), ('user2', 5, 222), ('user3', 333)]
+users = ['user1', 'user2', 'user3']
+data = list(enumerate(users))
+print(data) # [(0, 'user1'), (1, 'user2'), (2, 'user3))]
