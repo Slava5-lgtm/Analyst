7d95995dacd19e8f86a0daeb15df1fd6ce254fc0
Вячеслав <vaceslavsergunin@gmail.com>
2024-09-25 01:36:10 +0500

    +

--- Знакомство с языком Python. Обучение в записи/Урок 12. Семинар. Работа с файлами/Task_1/logger.py
+++ Знакомство с языком Python. Обучение в записи/Урок 12. Семинар. Работа с файлами/Task_1/logger.py
@@ -1,8 +1,22 @@
 
+from data_create import name_data, surname_data, phone_data, address_data
+
+
 # Соотавесвено в это файле logger.py. Создадим две функции
 
-def input_data (): # Функции input_data и пока обозначим как пустым ()
-    pass
+def input_data(): # Функции input_data и пока обозначим как пустым ()
+    name = name_data() 
+    surname = surname_data() 
+    phone = phone_data()
+    address = address_data()
+    var = int(input(f"В каком фрмате записать данные\n\n"
+    f"1 Вариант: \n"
+    f"{name}\n{surname}\n{phone}\n{address}\n\n"
+    f"2 Вариант: \n"
+    f"{name};{surname};{phone};{address}\n"
+    f"Выбирите вариант: "))
 
 def print_data (): # Функции print_data и пока обозначим как пустым ()
     pass
+
+input_data()
